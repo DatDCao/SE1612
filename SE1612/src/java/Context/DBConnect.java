ee81c3e662019b7536153338225c6e6fb0da0ab6
DatDCao <docaodat432001@gmail.com>
2022-03-24 04:27:34 +0700

    .

--- SE1612/src/java/Context/DBConnect.java
+++ SE1612/src/java/Context/DBConnect.java
@@ -24,7 +24,7 @@ public class DBConnect {
     /*Insert your other code right after this comment*/
  /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
     private final String serverName = "localhost";
-    private final String dbName = "Shopping";
+    private final String dbName = "DOBOOKSQL";
     private final String portNumber = "1433";
     private final String userID = "sa";
     private final String password = "123456";
